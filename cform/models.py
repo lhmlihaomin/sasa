55e2830c497c2286a1cc35c892e587cbd9a18053
Li Haomin <lhmlihaomin@live.com>
2016-09-08 14:11:34 +0800

    cform models skeleton

--- cform/models.py
+++ cform/models.py
@@ -20,7 +20,7 @@ class CField(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200, default="", blank=True)
     title = models.CharField(max_length=100)
-    field_type = models.charfield(max_length=100, choices=FIELD_TYPES)
+    field_type = models.CharField(max_length=100, choices=FIELD_TYPES)
     required = models.BooleanField(default=False)
     options = models.TextField(default="", blank=True)
 
@@ -29,4 +29,28 @@ class CField(models.Model):
 
 
 class CFormType(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, default="", blank=True)
+    fields = models.ManyToManyField(to=CField)
+    field_order = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+
+class CForm(models.Model):
+    name = models.CharField(max_length=100)
+    form_type = models.ForeignKey(CFormType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class CFormFieldValue(models.Model):
+    form = models.ForeignKey(CForm, on_delete=models.CASCADE)
+    field = models.ForeignKey(CField, on_delete=models.CASCADE)
+    value = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return ": ".join([self.form.name, self.field.name])
